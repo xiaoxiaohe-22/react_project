2607191fe2185c04a7a81eb2a10d76d928b592d1
xiaoxiaohe-22 <18602885377@163.com>
2021-01-05 16:55:08 +0800

    完成了login页面

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import  React,{Component} from "react";
+import {Switch,Route} from "react-router-dom"
+import Login from "./pages/login/login";
+import Admin from "./pages/admin/admin";
+export  default  class App extends Component{
+    render() {
+        return (
+            <div id="app">
+        <Switch>
+            <Route path="/login" component={Login}/>
+            <Route path="/admin" component={Admin}/>
+        </Switch>
+            </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        );
+    }
 }
-
-export default App;
---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,9 @@
+import  React,{Component} from "react"; //注意这里不是解构赋值
+
+export  default  class Admin extends Component{
+    render() {
+        return (
+            <h1>admin</h1>
+        );
+    }
+}
