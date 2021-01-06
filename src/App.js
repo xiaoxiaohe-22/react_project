1d846fe0b7944fb0fb7565cd20ccbd752ade1044
xiaoxiaohe-22 <18602885377@163.com>
2021-01-06 09:06:42 +0800

    reduc  first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import  React,{Component} from "react";
 import {Switch,Route} from "react-router-dom"
-import Login from "./pages/login/login";
-import Admin from "./pages/admin/admin";
 export  default  class App extends Component{
     render() {
         return (
             <div id="app">
-        <Switch>
-            <Route path="/login" component={Login}/>
-            <Route path="/admin" component={Admin}/>
-        </Switch>
+                <h1>APP</h1>
             </div>
 
         );
--- src/pages/admin/admin.jsx
+++
@@ -1,9 +0,0 @@
-import  React,{Component} from "react"; //注意这里不是解构赋值
-
-export  default  class Admin extends Component{
-    render() {
-        return (
-            <h1>admin</h1>
-        );
-    }
-}
